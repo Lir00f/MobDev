61c52b344320eea244b0e5075a73cc391d41046a
edemkaaa <113183510+edemkaaa@users.noreply.github.com>
2024-04-22 23:27:10 +0300

    commit

--- lib/screens/registered_content.dart
+++ lib/screens/registered_content.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RegisteredContent extends StatelessWidget {
+  const RegisteredContent({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Text('Контент для ЗАРЕГИСТРИРОВАННЫХ в системе');
--- lib/screens/unregistered_content.dart
+++ lib/screens/unregistered_content.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class UnregisteredContent extends StatelessWidget {
+  const UnregisteredContent({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Text("Контент для НЕ зарегистрированных в системе");
