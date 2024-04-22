1c8300bbc51eb2d355d241133d2a432ea694e10f
Lir00f <nikolaenko.e.i.21@gmail.com>
2024-04-22 22:06:47 +0300

    adding/storing/viewing works

--- lib/screens/registered_content.dart
+++ lib/screens/registered_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'add_car_form.dart';
 
 class RegisteredContent extends StatelessWidget {
@@ -23,7 +24,7 @@ class RegisteredContent extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => AddCarForm()),
+              MaterialPageRoute(builder: (context) => RentCarPage()),
             );
           },
           child: Text('Я хочу снять машину'),
@@ -31,4 +32,64 @@ class RegisteredContent extends StatelessWidget {
       ],
     );
   }
+}
+
+
+
+class RentCarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Снять машину'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('cars').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Ошибка: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+              return CarCard(data: data);
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CarCard extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  const CarCard({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(data['brand'] + ' ' + data['model']),
+        subtitle: Text('Пробег: ${data['mileage']} км'),
+        trailing: ElevatedButton(
+          onPressed: () {
+            // Implement functionality to rent the car
+          },
+          child: Text('Собрать в аренду'),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: RegisteredContent(),
+  ));
 }
