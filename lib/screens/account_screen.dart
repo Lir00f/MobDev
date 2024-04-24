ca9c8c5cf161b02c46d2e208969f781559880940
Lir00f <nikolaenko.e.i.21@gmail.com>
2024-04-25 00:48:45 +0300

    account page improves

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -19,40 +21,104 @@ class _AccountScreenState extends State<AccountScreen> {
     navigator.pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
   }
 
+  Future<List<Map<String, dynamic>>> getRentedCars() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final rentalDocs = await FirebaseFirestore.instance.collection('rentals')
+        .where('email', isEqualTo: user?.email)
+        .get();
+
+    List<Map<String, dynamic>> rentedCars = [];
+    rentalDocs.docs.forEach((doc) {
+      rentedCars.add(doc.data());
+    });
+
+    return rentedCars;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-          ),
-        ),
-        title: const Text('Аккаунт'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            tooltip: 'Open shopping cart',
-            onPressed: () => signOut(),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Ваш Email: ${user?.email}'),
-            TextButton(
-              onPressed: () => signOut(),
-              child: const Text('Выйти'),
+    return FutureBuilder(
+      future: getRentedCars(),
+      builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Ошибка: ${snapshot.error}'));
+        } else {
+          final rentedCars = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Аккаунт'),
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.logout),
+                  tooltip: 'Выйти из аккаунта',
+                  onPressed: () => signOut(),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Ваш Email: ${user?.email}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  if (rentedCars.isEmpty)
+                    Text(
+                      'У вас нет взятых в аренду автомобилей',
+                      style: TextStyle(fontSize: 16),
+                    )
+                  else
+                    ...rentedCars.map((car) {
+                      final startDate = (car['startDate'] as Timestamp).toDate();
+                      final endDate = (car['endDate'] as Timestamp).toDate();
+                      final carName = car['carName'];
+
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Машина: $carName',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 8),
+                              Text('Дата начала аренды: ${DateFormat('dd.MM.yyyy').format(startDate)}'),
+                              Text('Дата окончания аренды: ${DateFormat('dd.MM.yyyy').format(endDate)}'),
+                              SizedBox(height: 8),
+                              Text(
+                                'Можно оплатить и забрать на нашем автопарке по адресу: ул. Джеппар Акима 36',
+                                style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => signOut(),
+                    child: const Text('Выйти из аккаунта'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
